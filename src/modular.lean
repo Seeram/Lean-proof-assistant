04141791d307b9b8216a93a60ccace8e4ffd7a9d
Matus Behun <matus.behun@eurowag.com>
2021-02-19 11:36:19 +0100

    to_lattice definition

--- src/modular.lean
+++ src/modular.lean
@@ -6,13 +6,13 @@ universe u
 class modular_lattice(α : Type u) extends lattice α :=
   (modular_law: ∀ (x u v : α ), (x ≤ u) → u ⊓ (v ⊔ x) = (u ⊓ v) ⊔ x )
 
-theorem modular_lattice_isomorphism { α: Type u } [ modular_lattice α ] { u v w x y : α}: 
+theorem modular_lattice_isomorphism { α: Type u } [ modular_lattice α ] { u v w x y : α}:
   x ≤ u →
   x ≥ v →
   x ≥ u ⊓ v →
-  x ≤ u ⊔ v → 
+  x ≤ u ⊔ v →
   u ⊓ ( v ⊔ x ) = x ∧ (u ⊓ x) ⊔ v = x
-  := 
+  :=
   begin
     intros h1 h2 h3 h4,
     split,
@@ -28,24 +28,3 @@ theorem modular_lattice_isomorphism { α: Type u } [ modular_lattice α ] { u v 
       exact h2
     }
   end
-
-
-  
-
-
-
-
- 
-
-
-
-
- 
-
-
-
-
-
-
-
-
--- src/sublattice.lean
+++ src/sublattice.lean
@@ -1,7 +1,4 @@
-import tactic
 import order.lattice
-
-
 variables { L : Type* } [ lattice L]
 
 set_option old_structure_cmd true
@@ -13,21 +10,22 @@ structure sublattice ( L : Type* ) [lattice L] :=
 
 namespace sublattice
 
-instance : has_coe ( sublattice L ) (set L) :=
+instance : has_coe ( sublattice L ) (set L)
+:=
   { coe := sublattice.carrier }
 
-instance : has_mem L ( sublattice L ) :=
+instance : has_mem L ( sublattice L )
+:=
   ⟨ λ m S, m ∈ ( S : set L) ⟩
 
-instance : has_coe_to_sort ( sublattice L ) :=
+instance : has_coe_to_sort ( sublattice L )
+:=
   ⟨ Type*, λ S, { x : L // x ∈ S } ⟩
 
 lemma mem_coe {SL : sublattice L} {x : L} :
   x ∈ SL.carrier ↔ x ∈ SL
 :=
   iff.rfl
-
-end sublattice
 
 protected lemma sublattice.exists {SL : sublattice L} {p : SL → Prop} :
   (∃ x : SL, p x) ↔ ∃ x ∈ SL, p ⟨x, ‹x ∈ SL›⟩
@@ -48,8 +46,6 @@ theorem ext {A B : sublattice L} (h : ∀ x, x ∈ A ↔ x ∈ B) :
   A = B
 :=
   ext' $ set.ext h
-
-namespace sublattice
 
 def copy (SL : sublattice L) (S : set L) (hs : S = SL) :
   sublattice L
@@ -72,46 +68,60 @@ lemma copy_eq {s : set L} (hs : s = SL) :
 :=
   ext' hs
 
+variable (SL)
+
+theorem inf_mem {a b : L} :
+  a ∈ SL → b ∈ SL → a ⊓ b ∈ SL
+:=
+  sublattice.inf_mem' SL
+
+theorem sup_mem {a b : L} :
+  a ∈ SL → b ∈ SL → a ⊔ b ∈ SL
+:=
+  sublattice.sup_mem' SL
+
+lemma coe_injective :
+  function.injective (coe : SL → L)
+:=
+  subtype.val_injective
+
+instance to_lattice {M : Type*} [lattice L] (SL : sublattice L) :
+  lattice L
+:=
+ SL.coe_injective.lattice
+
 end sublattice
 
-/-
-class is_mul_hom {α β : Type*} [has_mul α] [has_mul β] (f : α → β) : Prop :=
-(map_mul [] : ∀ x y, f (x * y) = f x * f y)
--/
 
-/-
-@[to_additive]
-class is_monoid_hom [monoid α] [monoid β] (f : α → β) extends is_mul_hom f : Prop :=
-(map_one [] : f 1 = 1)
--/
+variables [lattice α] {a b c d : α}
 
-/-
-namespace is_monoid_hom
+open function
 
-variables [monoid α] [monoid β] (f : α → β) [is_monoid_hom f]
+protected def function.injective.lattice { β : Type*}
+  [ has_inf β ] [ has_sup β ]
+  (f : β → α )
+  (hf: injective f)
+  (inf : ∀ a b, f(a ⊓ b) = f(a) ⊓ f(b) )
+  (sup : ∀ a b, f(a ⊔ b) = f(a) ⊔ f(b) )
+  :
+  lattice α
+:=
+  {
+    le           := ( ≤ ),
+    le_refl      := λ x, le_refl x,
+    le_trans     := λ x y z, le_trans,
+    le_antisymm  := ,
+--    le_antisymm  := λ x y hx hy, ,
 
--- A monoid homomorphism preserves multiplication.
-@[to_additive]
-lemma map_mul (x y) : f (x * y) = f x * f y :=
-is_mul_hom.map_mul f x y
+    sup          := ( ⊔ ),
+    le_sup_left  := λ x y, show x ≤ _, from ,
+    le_sup_right := λ x y, show y ≤ _, from ,
+    sup_le       := λ x y z hxz hyz, lfp_le $ sup_le (sup_le hxz hyz) (z.2.symm ▸ le_refl z.1),
 
-end is_monoid_hom
--/
+    inf          := ( ⊓ ),
+    inf_le_left  := λ x y, show _ ≤ x, from ,
+    inf_le_right := λ x y, show _ ≤ y, from ,
+    le_inf       := λ x y z hxy hxz, ,
+  }
 
-/-
-namespace is_monoid_hom
-
-variables [monoid α] [monoid β] (f : α → β) [is_monoid_hom f]
-
-/-- The identity map is a monoid homomorphism. -/
-@[to_additive]
-instance id : is_monoid_hom (@id α) := { map_one := rfl }
-
-/-- The composite of two monoid homomorphisms is a monoid homomorphism. -/
-@[to_additive] -- see Note [no instance on morphisms]
-lemma comp {γ} [monoid γ] (g : β → γ) [is_monoid_hom g] :
-  is_monoid_hom (g ∘ f) :=
-{ map_one := show g _ = 1, by rw [map_one f, map_one g], ..is_mul_hom.comp _ _ }
-
-end is_monoid_hom
--/
+end lattice
